540b6e09f5a4e58ceba81422e8bb4288438c35c8
Jochum Sloot <67738459+Jsloot1986@users.noreply.github.com>
2020-10-17 15:44:52 +0200

    een const. maken in javascript
    een const. maken voor een café waar word gecontroleerd of de persoon boven 18 is, een vrouw is, en de bob is.

---
+++ opdracht_1/script.js
@@ -0,0 +1,17 @@
+const age = 21;
+const isFemale = true;
+const driverStatus = 'geen bob';
+if (age > 17)
+{console.log ("Welkom je bent boven de 18");
+}
+else
+{console.log ("Helaas je bent nog geen 18 jaar");
+}
+if (isFemale == true)
+{console.log ("Welkom je hebt toegang tot de ladiesnight");
+}
+else {console.log ("Helaas deze avond is alleen toegangelijk voor dames");}
+if (driverStatus=='bob')
+{console.log ("U mag nog rijden");
+}
+else {console.log ("Helaas, kunnen we een taxi voor uw bestellen of bent u met iemand anders die bob is?");}
