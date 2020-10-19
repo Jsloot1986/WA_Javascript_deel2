68df611e9e87423f675b696d9471a2dc0bf14dfd
Jochum Sloot <67738459+Jsloot1986@users.noreply.github.com>
2020-10-19 17:41:47 +0200

    level 14 opdracht 3 for loop
    werken met de for loop in verschillende situaties

---
+++ JS_level_14/opdracht_3/multiplucation-tables.js
@@ -0,0 +1,12 @@
+for (let i=1; i<=10; i++){
+    for (let j=9; j<=9; j++){
+        if (result=i*j)
+        console.log (i +'x'+ j + '=' + result);
+    }
+}
+for (let i=1; i<=10; i++){
+    for (let j=1; j<=10; j++){
+        if (result=i*j)
+        console.log (i +'x'+ j + '=' + result);
+    }
+}
