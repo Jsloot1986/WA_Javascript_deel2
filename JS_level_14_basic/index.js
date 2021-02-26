0ecf299477dae940c8c4270bc999a46f56e69b38
Jochum Sloot <67738459+Jsloot1986@users.noreply.github.com>
2021-02-26 12:23:47 +0100

    basic javascript function

---
+++ JS_level_14_basic/index.js
@@ -0,0 +1,25 @@
+const makePizza = function () {
+    console.log("how to make pizza?")
+    console.log("step 1: get the dough")
+    console.log("step 2: Get the toppings")
+    console.log("put tomatosauce on the pizza")
+    console.log("put the toppings")
+    console.log("put the pizza 20 min on 220 degreed in the oven")
+    console.log("enjoy!")
+};
+
+const makeSushi = function () {
+    console.log("how to make sushi")
+    console.log("put the some rice on the seaweed")
+    console.log("put the topping(like cumcumber, salmon, tuna")
+    console.log("roll the seaweed")
+    console.log("enjoy")
+}
+const orderSushi = 3;
+const orderPizza = 2;
+
+makeSushi();
+makePizza();
+makeSushi();
+makePizza();
+makeSushi();
