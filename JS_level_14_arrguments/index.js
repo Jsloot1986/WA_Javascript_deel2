262ddf103d5263302c6d5406909dba1bf52fd7b8
Jochum Sloot <67738459+Jsloot1986@users.noreply.github.com>
2021-02-27 09:48:07 +0100

    function arrguments

---
+++ JS_level_14_arrguments/index.js
@@ -0,0 +1,12 @@
+const paintingTheWalls = function (wallside, color) {
+    console.log("the " + wallside + "wall is painted " + color)
+}
+
+const greencolor = "green";
+const orangecolor = "orange";
+
+const wallEast = "east-west";
+const wallNorth = "north-east";
+
+paintingTheWalls(wallEast, greencolor);
+paintingTheWalls(wallNorth, orangecolor);
